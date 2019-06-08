e22a00a23de38e5d4d9255c4fa0bd504ca413132
aidananderson <aidananderson@uvic.ca>
2019-06-07 19:47:27 -0700

    cleaned up assembler logic

--- assembler.py
+++ assembler.py
@@ -1,3 +1,4 @@
+import itertools
 instruction = {
             'store':[0, 1, 0, 0, 0],
             'storeIndr':[0, 1, 1, 0, 0],
@@ -8,7 +9,8 @@ instruction = {
             'subi':[1, 0, 0, 0, 1],
             'subIndr': [0, 0, 1, 0, 1],
             'clear':[1, 0, 0, 1, 1],
-            'branch':[1, 0, 0, 1, 0]}
+            'branch':[1, 0, 0, 1, 0]
+            }
 
 def to_bin(d):
     if(d[0] == '-'):
@@ -18,45 +20,51 @@ def to_bin(d):
     zero[-len(b):] = b
     return zero
 
-def isInt(i):
+
+
+def is_(i, f):
     try:
-        val = int(i)
+        val = f(i)
     except ValueError:
         return False
     return True
+isInt = lambda i : is_(i, int)
+isNumeric = lambda i : is_(i, float)
 
 def assemble(assembly):
     b_instructions = []
-    i = 0
     define_dict = {}
-    for l in assembly:
-        i +=1
+    
+    for l, i in zip(assembly, itertools.count(1)):
         if(len(l) <= 1):
             continue
         words = l.split('\n')[0].split(' ')
         words = list(filter(lambda x : x is not '', words))
-        if words[0] not in instruction:
-            if(words[0] == '#define'):
+        if words[0] not in instruction: # not builtin instruction
+            if(words[0] == '#define'):  # check for previously defined token
                 if len(words) < 3:
-                    raise SyntaxError('Invalid assembly syntax on line {}. Missing arguments for #define. Must have token followed by constant.'.format(i))
-                if isInt(words[1]):
+                    raise SyntaxError('Invalid assembly syntax on line {}. Missing arguments for #define. Must have name followed by constant.'.format(i))
+                elif isNumeric(words[1]):
                     raise SyntaxError('Invalid assembly syntax on line {}. #define name \'{}\' cannot be numeric.'.format(i, words[1])) 
-                if not isInt(words[2]):
-                    raise SyntaxError('Invalid assembly syntax on line {}. #define value \'{}\' must be numeric.'.format(i, words[2]))
-                define_dict[words[1]] = to_bin(words[2])  
-            else:
+                elif not isInt(words[2]):
+                    raise SyntaxError('Invalid assembly syntax on line {}. #define value \'{}\' must be an integer.'.format(i, words[2]))
+                else:
+                    define_dict[words[1]] = to_bin(words[2])  
+            else: 
                 raise SyntaxError('Invalid assembly syntax on line {}. Token \'{}\' is not recognized.'.format(i, words[0]))
         
-        elif(len(words) > 1 and words[1] in define_dict):
-            b_instructions += [instruction[words[0]] + define_dict[words[1]]]        
-        else:
-            if len(words) > 1 and not isInt(words[1]):
-                raise SyntaxError('Invalid assembly syntax on line {}. Argument \'{}\' must be numeric.'.format(i, words[1]))
-            try:
-                arg = to_bin(words[1])
-            except:
-                arg = [0]*8
-            b_instructions += [instruction[words[0]] + arg]
+        elif len(words) > 1: # recognized instruction with at least one argument
+            if words[1] in define_dict:
+                b_instructions += [instruction[words[0]] + define_dict[words[1]]]        
+            elif not isInt(words[1]): #argument must be integer, since other cases are handled above
+                raise SyntaxError('Invalid assembly syntax on line {}. Argument \'{}\' must be an integer.'.format(i, words[1]))
+            else: # argument is an integer, so the following will work
+                b_instructions += [instruction[words[0]] + to_bin(words[1])]
+        else: # if no argument is given, fill with zeros
+              # this might be unintended, however, if the programmer is aware of this
+              # s/he can use this to simplify code.
+              # e.g. nop can be achieved with 'addi'
+            b_instructions += [instruction[words[0]] + [0]*8]
 
     return b_instructions    
 
