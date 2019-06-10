bd37f2f1370ce97034113658422a07908a316b10
aidananderson <aidananderson@uvic.ca>
2019-06-09 19:59:50 -0700

    added static expression evaluation

--- assembler.py
+++ assembler.py
@@ -1,15 +1,15 @@
 import itertools
 instruction = {
-            'store':[0, 1, 0, 0, 0],
+            'store':    [0, 1, 0, 0, 0],
             'storeIndr':[0, 1, 1, 0, 0],
-            'add': [0, 0, 0, 0, 0],
-            'addi': [1, 0, 0, 0, 0],
-            'addIndr':[0, 0, 1, 0, 0],
-            'sub': [0, 0 ,0, 0, 1],
-            'subi':[1, 0, 0, 0, 1],
-            'subIndr': [0, 0, 1, 0, 1],
-            'clear':[1, 0, 0, 1, 1],
-            'branch':[1, 0, 0, 1, 0]
+            'add':      [0, 0, 0, 0, 0],
+            'addi':     [1, 0, 0, 0, 0],
+            'addIndr':  [0, 0, 1, 0, 0],
+            'sub':      [0, 0 ,0, 0, 1],
+            'subi':     [1, 0, 0, 0, 1],
+            'subIndr':  [0, 0, 1, 0, 1],
+            'clear':    [1, 0, 0, 1, 1],
+            'branch':   [1, 0, 0, 1, 0]
             }
 
 def to_bin(d):
@@ -19,8 +19,6 @@ def to_bin(d):
     zero = [0 for i in range(8)]
     zero[-len(b):] = b
     return zero
-
-
 
 def is_(i, f):
     try:
@@ -33,40 +31,72 @@ isNumeric = lambda i : is_(i, float)
 
 def assemble(assembly):
     b_instructions = []
-    define_dict = {}
-    
+    eval_dict = {'len' : len, 'dir' : dir, 'PC' : 0}
+    num_builtin = len(eval_dict) + 1
+    eval_dict['num_builtin'] = num_builtin
     for l, i in zip(assembly, itertools.count(1)):
+        
         if(len(l) <= 1):
             continue
         words = l.split('\n')[0].split(' ')
         words = list(filter(lambda x : x is not '', words))
+        if words[0][0] is '#':
+            continue
         if words[0] not in instruction: # not builtin instruction
-            if(words[0] == '#define'):  # check for previously defined token
+            if(words[0] == 'DEFINE'):  # check for previously defined token
                 if len(words) < 3:
                     raise SyntaxError('Invalid assembly syntax on line {}. Missing arguments for #define. Must have name followed by constant.'.format(i))
-                elif isNumeric(words[1]):
+                expr_str = ' '.join(words[2:])
+                try:
+                    expr = str(eval(expr_str, {'__builtins__' : None}, eval_dict))
+                except TypeError:
+                    raise SyntaxError('Invalid expression on line {}.'.format(i))
+                
+                if isNumeric(words[1]):
                     raise SyntaxError('Invalid assembly syntax on line {}. #define name \'{}\' cannot be numeric.'.format(i, words[1])) 
-                elif not isInt(words[2]):
-                    raise SyntaxError('Invalid assembly syntax on line {}. #define value \'{}\' must be an integer.'.format(i, words[2]))
-                else:
-                    define_dict[words[1]] = to_bin(words[2])  
+                elif not isInt(expr):
+                    raise SyntaxError('Invalid assembly syntax on line {}. #define value \'{}\' must be an integer.'.format(i, expr))
+                else:     
+                    eval_dict[words[1]] = int(expr)
             else: 
                 raise SyntaxError('Invalid assembly syntax on line {}. Token \'{}\' is not recognized.'.format(i, words[0]))
         
-        elif len(words) > 1: # recognized instruction with at least one argument
-            if words[1] in define_dict:
-                b_instructions += [instruction[words[0]] + define_dict[words[1]]]        
-            elif not isInt(words[1]): #argument must be integer, since other cases are handled above
-                raise SyntaxError('Invalid assembly syntax on line {}. Argument \'{}\' must be an integer.'.format(i, words[1]))
-            else: # argument is an integer, so the following will work
-                b_instructions += [instruction[words[0]] + to_bin(words[1])]
-        else: # if no argument is given, fill with zeros
-              # this might be unintended, however, if the programmer is aware of this
-              # s/he can use this to simplify code.
-              # e.g. nop can be achieved with 'addi'
-            b_instructions += [instruction[words[0]] + [0]*8]
+        else: 
+            instr = [instruction[words[0]] + [0]*8]
+            if len(words) > 1 and words[1][0] is not '#': # recognized instruction with at least one argument     
+                expr_str = ' '.join(words[1:])
+                try:
+                    
+                    expr = str(eval(expr_str, {'__builtins__' : None}, eval_dict))
+                    if not isInt(expr):
+                        raise SyntaxError('Invalid expression on line {}. Expression {} evaluates to {} and not to an integer.'.format(i, expr_str, expr))
+                    
+                    if words[0] == 'branch':
+                        expr = str(int(expr) - 2)
+                        
+                    instr = [instruction[words[0]] + to_bin(expr)]
+                except TypeError:
+  
+                    instr = [['i', i, words[0], expr_str]] 
+            b_instructions += instr
+            eval_dict['PC'] += 1
+    final_instr = []
 
-    return b_instructions    
+    for instr in b_instructions:
+        if instr[0] is not 'i':
+            final_instr += [instr]
+        else:
+            
+            try:
+                expr = str(eval(instr[3], {'__builtins__' : None}, eval_dict))
+            except TypeError:            
+                raise SyntaxError('Invalid expression on line {}.'.format(instr[1]))
+            if not isInt(expr):
+                raise SyntaxError('Invalid expression on line {}. Expression {} evaluates to {} and not to an integer.'.format(i, expr_str, expr))
+            if instr[2] == 'branch':
+                expr = str(int(expr) - 2)
+            final_instr += [instruction[instr[2]] + to_bin(expr)] 
+    return final_instr   
 
 def main():
     a_file = sys.argv[1]
